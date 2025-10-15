9bd3811cd1eee4943dc5323bc14d0589940f4075
MateHUNAa <matehunter123@gmail.com>
2025-10-15 08:26:07 +0200

    feat(meltbot): meltbot configuration

--- Core/Models/AppConfig.cs
+++ Core/Models/AppConfig.cs
@@ -1,7 +1,8 @@
-﻿using mMacro.Core.Functions;
+﻿using mMacro.Core.Functions.Inventory;
 using mMacro.Core.Managers;
-using System.Numerics;
 using mMacro.Core.Utils;
+using System.Numerics;
+using static mMacro.Core.Utils.PixelUtils;
 
 namespace mMacro.Core.Models
 {
@@ -22,12 +23,12 @@ namespace mMacro.Core.Models
         public ScanType ScanType { get; set; } = ScanType.Simple;
         public int scanLevel { get; set; } = 2;
         public Dictionary<string, PixelUtils.ColorRange> ColorRanges { get; set; } = new() {
-            { "Blue", new PixelUtils.ColorRange { R = (55, 57), G = (113, 117), B = (166, 173) } },
-            { "Purple", new PixelUtils.ColorRange { R = (150, 156), G = (42, 44), B = (79, 82) } },
-            { "Gold", new PixelUtils.ColorRange { R = (254, 255), G = (161, 162), B = (73, 75) } },
-            { "Unique", new PixelUtils.ColorRange { R = (249, 256), G = (248, 255), B = (97, 107) } },
-            { "Set", new PixelUtils.ColorRange  { R = (38,78), G = (188, 199), B = (223, 227) } },
-            { "Color Item", new PixelUtils.ColorRange { R = (120, 131), G = (96, 107), B = (66, 77) } }
+          { "Blue", new ColorRange { R = (55, 60), G = (113, 120), B = (166, 179) } },
+            { "Purple", new ColorRange { R = (150, 162), G = (42, 47), B = (79, 85) } },
+            { "Gold", new ColorRange { R = (254, 255), G = (161, 170), B = (73, 77) } },
+            { "Unique", new ColorRange { R = (248, 256), G = (248, 255), B = (40, 60) } },
+            { "Set", new ColorRange { R = (38,78), G = (188, 199), B = (223, 227) } },
+            { "Color Item", new ColorRange { R = (120, 131), G = (96, 107), B = (66, 77) } }
         };
 
         // ================== SwapCape ==================
@@ -37,5 +38,9 @@ namespace mMacro.Core.Models
         public int swapCol { get; set; } = 0;
         // ================== ReviveBot ==================
         public Vector2 FirstPlayerPos { get; set; } = Vector2.Zero;
+        // ================== MeltBot ==================
+        public Vector2 MeltFirstPos { get; set; } = Vector2.Zero;
+        public Vector2 MeltButtonPos { get; set; } = Vector2.Zero;
+        public Vector2 MeltCloseBtn { get; set; } = Vector2.Zero;
     }
 }
