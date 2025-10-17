524b60d68f52ace35ca7eb0f05536f853720b14e
MateHUNAa <matehunter123@gmail.com>
2025-10-17 03:55:30 +0200

    feat: settings for (capeswap, sellbot),

--- Core/Models/Settings.cs
+++ Core/Models/Settings.cs
@@ -9,15 +9,24 @@ namespace mMacro.Core.Models
             public int MeltItemClickDelay { get; set; } = 150;
             public int MeltGemClickDelay { get; set; } = 90;
             public int MeltGemTaskEndDelay { get; set; } = 2000;
+            public int SwapCapeClickDelay { get; set; } = 75;
         }
 
         public class Offsets
         {
             public int ColorTolarence { get; set; } = 5;
+            public int BagOffsetX { get; set; } = 49;
+        }
+
+        public class Sizes
+        {
+            public int CellSize { get; set; } = 76;
+            public int BagSize { get; set; } = 40;
         }
 
         public Timeings m_Timeings { get; set; } = new Timeings();
         public Offsets m_Offsets { get; set; } = new Offsets();
+        public Sizes m_Sizes { get; set; } = new Sizes();
 
         private static Settings? m_instance;
         public static Settings Instance
