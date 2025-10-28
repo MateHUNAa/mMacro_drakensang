acff1a094abd3a1399cb9a270b09a8a5ad60fee8
MateHUNAa <matehunter123@gmail.com>
2025-10-28 11:13:50 +0100

    feat: Percentage checking;

--- Core/Utils/PixelUtils.cs
+++ Core/Utils/PixelUtils.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace mMacro.Core.Utils
@@ -33,6 +34,38 @@ namespace mMacro.Core.Utils
 
             return colorRange.IsInRange(pixel);
         }
+        public static double GetPixelMatchPercentage(Color pixel, ColorRange colorRange)
+        {
+            return colorRange.GetMatchPercentage(pixel);
+        }
+
+        public static double GetPixelMatchPercentage(Color pixel, List<ColorRange> colorRanges)
+        {
+            if (colorRanges == null || colorRanges.Count == 0)
+                return 0;
+
+            return colorRanges.Max(c => c.GetMatchPercentage(pixel));
+        }
+
+        public static double GetPixelMatchPercentage(Color pixel, Dictionary<string, ColorRange> colorRanges)
+        {
+            if (colorRanges == null || colorRanges.Count == 0)
+                return 0;
+            return colorRanges.Values.Max(c => c.GetMatchPercentage(pixel));
+        }
+
+        public static double GetPixelMatchPercentage(Color pixel, Color color)
+        {
+            ColorRange colorRange = new ColorRange
+            {
+                R = (color.R - 1, color.R + 1),
+                G = (color.G - 1, color.G + 1),
+                B = (color.B - 1, color.B + 1)
+            };
+
+            return colorRange.GetMatchPercentage(pixel);
+        }
+
         public static Color GetPixelColor(int x, int y)
         {
             using var bmp = new Bitmap(1, 1);
@@ -73,6 +106,28 @@ namespace mMacro.Core.Utils
                 return color.R >= R.Min-1 && color.R <= R.Max+1 &&
                        color.G >= G.Min-1 && color.G <= G.Max+1 &&
                        color.B >= B.Min-1 && color.B <= B.Max+1;
+            }
+
+            public double GetMatchPercentage(Color color)
+            {
+                double rScore = GetChannelMatch(color.R, R.Min, R.Max);
+                double gScore = GetChannelMatch(color.G, G.Min, G.Max);
+                double bScore = GetChannelMatch(color.B, B.Min, B.Max);
+
+                return (rScore + gScore + bScore) / 3.0;
+            }
+            private double GetChannelMatch(int value, int min, int max)
+            {
+                if (value >= min && value <= max)
+                    return 100.0;
+
+                int rangeSize = max - min;
+                int distance = value < min ? min - value : value - max;
+
+                double penalty = (double)distance / (rangeSize + 1);
+                double match = 100.0 * Math.Max(0,1 - penalty);
+
+                return match;
             }
         }
     }
